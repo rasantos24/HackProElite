34ac9d286271989f194f06a7ffb1adfeff97f70d
Rafael Santos <rafa.santos.24@unitec.edu>
2015-11-23 22:11:34 -0600

    Cambios

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HackProElite.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        // GET: Register
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        public ActionResult AdminIndex()
+        {
+            return View();
+        }
+    }
+
+    }
